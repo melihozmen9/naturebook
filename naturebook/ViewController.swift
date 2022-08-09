797a0a41a250c971b374884c9eb6df9a5549bb85
Melih Özmen <melihozmen9@gmail.com>
2022-08-09 13:56:02 +0300

    NatureBook1
    Tasarım Yapıldı.

--- naturebook/ViewController.swift
+++ naturebook/ViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "HiCoreData"
+        return cell
+    }
+    
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableview.delegate = self
+        tableview.dataSource = self
     }
 
 
---
+++ naturebook/ViewController2.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController2.swift
+//  naturebook
+//
+//  Created by Kullanici on 9.08.2022.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    @IBOutlet weak var nametext: UITextField!
+    @IBOutlet weak var placetext: UITextField!
+    @IBOutlet weak var yeartext: UITextField!
+    @IBOutlet weak var imageview: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveclickedbutton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
