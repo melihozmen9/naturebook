6fa97e8ab1981de57db0356df48a846290d7aef5
Melih Özmen <melihozmen9@gmail.com>
2022-08-16 23:39:48 +0300

    naturebook-duzenleme3
    core data veri çekme işlemleri

--- naturebook/ViewController.swift
+++ naturebook/ViewController.swift
@@ -6,15 +6,19 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
+    var namearray = [String]()
+    var idarray = [UUID]()
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return namearray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "HiCoreData"
+        cell.textLabel?.text = namearray[indexPath.row]
         return cell
         
     }
@@ -27,6 +31,27 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         tableview.delegate = self
         tableview.dataSource = self
         
+    }
+    func getdata() {
+        guard let appdelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        let context = appdelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Galeri")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+           let results =  try context.fetch(fetchRequest)
+            for result in results as! [NSManagedObject]{
+                if let name = result.value(forKey: "name") as? String{
+                    self.namearray.append(name)
+                }
+                
+                if let id = result.value(forKey: "id") as? UUID{
+                    self.idarray.append(id)
+                }
+            }
+        } catch  {
+            
+        }
     }
     @objc func addItem () {
         performSegue(withIdentifier: "tosecondvc", sender: nil)
